0191e00c3072a513497c1b59f06dc6e181bbfd2d
David <djorgepbl@gmail.com>
2017-05-09 19:22:39 +0100

    bugs correct

--- ementaHoje.py
+++ ementaHoje.py
@@ -12,9 +12,10 @@ from datetime import date
 import shelve
 import os
 import dialogs
+from datetime import timedelta
 
 last_ementa=None
-export_text=True
+export_text=False
 shift_feriado=0
 dias_feriado =[]
 tipo_prato = ['Frito','Assado', 'Grelhado' , 'Cozido', 'Gratin.', 'Estufado','estufado','Grelh.']
@@ -24,8 +25,9 @@ ignorar = '''Nota: Os Pratos confecionados nesta ementa semanal podem conter os 
 crustáceos, ovos e produtos à base de ovos, peixes e produtos à base de peixe, amendoins e produtos à base de amendoins, soja e produtos à base de soja, leite e produtos à base de leite, 
 frutos de casca rija e produtos à base destes frutos, aipo e produtos à base de aipo, mostarda e produtos à base de mostarda, sementes de sésamo e produtos à base de sementes de 
 sésamo, dióxido de enxofre e sulfitos, tremoço e produtos à base tremoço, moluscos e produtos à base de moluscos.'''
-ignorar2=['ª','Alfragide','ALMOÇO','Semana de']
+ignorar2=['ª','Alfragide','ALMOÇO','Semana de','Nota:']
 ignorar3='Semana de'
+ignorar4='''Nota: Os Pratos confecionados nesta ementa semanal podem conter os seguintes alergénios: cereais que contêm glúten e produtos à base destes cereais, crustáceos e produtos à base de crustáceos, ovos e produtos à base de ovos, peixes e produtos à base de peixe, amendoins e produtos à base de amendoins, soja e produtos à base de soja, leite e produtos à base de leite, frutos de casca rija e produtos à base destes frutos, aipo e produtos à base de aipo, mostarda e produtos à base de mostarda, sementes de sésamo e produtos à base de sementes de sésamo, dióxido de enxofre e sulfitos, tremoço e produtos à base tremoço, moluscos e produtos à base de moluscos.'''
 numtipo_prato =0
 numrefeicao=0
 numcaloria=0
@@ -49,17 +51,25 @@ def get_dia(diaint,mes,ano,item_count,item):
   if item in tipo_prato:
     item_max =4
 
-  if item_count > item_max:
-    diaint+=(item_count/item_max)
+  if item_count > item_max:   
+    #diaint+=(item_count/item_max)
+    dt = datetime.datetime(int(ano), int(mes), diaint)+datetime.timedelta(days=item_count/item_max)
+    diaint = dt.day
+    mesint = dt.month
     print '%d=(%d/%d)=%d'%(diaint,item_count,item_max, item_count/item_max)
     itemlinha= item_count % item_max
     if itemlinha == 0:
       itemlinha= item_max
-      diaint-=1
+      dt+=datetime.timedelta(days=-1)
+      diaint = dt.day
+      #diaint-=1
     while True:
-      diaint += shift_feriado
-      if date(int(ano),int(mes), diaint) in feriados:
-        print '++++++++++++++++++++ %d/%d/%d'%(int(ano),int(mes),diaint)
+      dt+= datetime.timedelta(days=shift_feriado)
+      diaint = dt.day
+      mesint = dt.month
+      #diaint += shift_feriado
+      if date(int(ano),mesint, diaint) in feriados:
+        print '++++++++++++++++++++ %d/%d/%d'%(int(ano),mesint,diaint)
         #dia[str(diaint)][ementalinha]['ementa'] = 'Feriado'
         shift_feriado+=1
       else:
@@ -93,6 +103,75 @@ def convert(fname, pages=None):
   output.close()
   return text
   
+def parseLine(line):
+    global numtipo_prato, decimal, ignorar, ignorar2, ignorar3, refeicao, diaint,ames,ano, numcaloria, numementa, numrefeicao, numtipo_prato, last_ementa, ementa
+    
+    line = line.strip()
+    if len(line.strip()) == 1:
+      return 
+    if len(line)==0:
+      return
+    if line.strip() in ignorar:
+      return
+    if line.strip() in ignorar2:
+      print 'ignorar2 matched. comtinue...'
+      return      
+    if line.strip() in ignorar4:
+      print 'ignorar4 matched. comtinue...'
+      return      
+    salta = False
+    for word in ignorar2:
+      if line.strip().startswith(word):
+          salta = True
+          break
+    if salta == True:              
+      return   
+
+    if line.strip() in tipo_prato:
+      numtipo_prato+=1
+      print line          
+      diaint = int(dedia)
+      (diaint,item_linha)=get_dia(diaint,demes,ano,numtipo_prato,line)
+      print "dia[%d][%d]['tipo_prato']"%(diaint,item_linha)
+      dia[str(diaint)][item_linha]['tipo_prato'] = line
+      return
+      
+    if line.strip() in refeicao:
+      numrefeicao+=1
+      diaint = int(dedia)
+      #print line 
+      (diaint,item_linha)=get_dia(diaint,demes,ano,numrefeicao,line)
+      print "dia[%d][%d]['refeicao']"%(diaint,item_linha)  
+      if diaint > maxdia:
+        '''refeicao tb existe em feriados
+         '''
+        print '%d (dia) > %d (maxdia)'%(diaint,maxdia)
+        return
+        
+      dia[str(diaint)][item_linha]['refeicao'] = line
+      return
+  
+    if line.strip().isdigit():
+      decimal+=1
+      diaint = int(dedia)
+      (diaint,item_linha)=get_dia(diaint,demes,ano,decimal,line)
+      print "dia[%d][%d]['calorias']"%(diaint,item_linha)  
+      dia[str(diaint)][item_linha]['calorias'] = line
+      return
+
+    if last_ementa == line:
+      '''duplicado, bug do parser pdf'''
+      return
+    ementa+=1
+    
+    diaint = int(dedia)
+    (diaint,ementalinha)=get_dia(diaint,demes,ano,ementa,line)
+    
+    
+    print "dia[%s][%d]['ementa']" % ( str(diaint), ementalinha)
+    dia[str(diaint)][ementalinha]['ementa'] = line
+    last_ementa = line
+  
 def get_pdf_from_user():
     pdf_files = [f for f in os.listdir(os.curdir) if f.endswith('.pdf')]
     return dialogs.list_dialog(title='pdf de ementa', items=pdf_files)
@@ -109,7 +188,7 @@ for pagenum in [0,1,2,3,4,5,6,7,8,9,10]:
 
   print('page-----------'+str(pagenum))
   page=[pagenum]
-  textPage = convert(file1, page)
+  textPage = convert(filename, page)
   if len(textPage) == 0 or 'Alfragide'   not in textPage:
     continue
   #print(text)
@@ -136,207 +215,57 @@ for pagenum in [0,1,2,3,4,5,6,7,8,9,10]:
       decimal=0
       ementa=0
       numrefeicao=0
-             
-      dia[str(diaint)]= {}
-      dia[str(diaint+1)]= {}
-      dia[str(diaint+2)]= {}
-      dia[str(diaint+3)]= {}
-      dia[str(diaint+4)]= {} 
-      dia[str(diaint)][ementa+5] = {}
-      dia[str(diaint)][ementa+1] = {}
-      dia[str(diaint)][ementa+2] = {}
-      dia[str(diaint)][ementa+3] = {}
-      dia[str(diaint)][ementa+4] = {}        
-      dia[str(diaint)][ementa+5]['tipo_prato'] = {}
-      dia[str(diaint)][ementa+1]['tipo_prato'] = {}
-      dia[str(diaint)][ementa+2]['tipo_prato'] = {}
-      dia[str(diaint)][ementa+3]['tipo_prato'] = {}
-      dia[str(diaint)][ementa+4]['tipo_prato'] = {}        
-      dia[str(diaint)][ementa+5]['calorias'] = {}
-      dia[str(diaint)][ementa+1]['calorias'] = {}
-      dia[str(diaint)][ementa+2]['calorias'] = {}
-      dia[str(diaint)][ementa+3]['calorias'] = {}
-      dia[str(diaint)][ementa+4]['calorias'] = {}
-      dia[str(diaint)][ementa+5]['refeicao'] = {}
-      dia[str(diaint)][ementa+1]['refeicao'] = {}
-      dia[str(diaint)][ementa+2]['refeicao'] = {}
-      dia[str(diaint)][ementa+3]['refeicao'] = {}
-      dia[str(diaint)][ementa+4]['refeicao '] = {}
-
-      dia[str(diaint+1)][ementa+5] = {}
-      dia[str(diaint+1)][ementa+1] = {}
-      dia[str(diaint+1)][ementa+2] = {}
-      dia[str(diaint+1)][ementa+3] = {}
-      dia[str(diaint+1)][ementa+4] = {}        
-      dia[str(diaint+1)][ementa+5]['tipo_prato'] = {}
-      dia[str(diaint+1)][ementa+1]['tipo_prato'] = {}
-      dia[str(diaint+1)][ementa+2]['tipo_prato'] = {}
-      dia[str(diaint+1)][ementa+3]['tipo_prato'] = {}
-      dia[str(diaint+1)][ementa+4]['tipo_prato'] = {}        
-      dia[str(diaint+1)][ementa+5]['calorias'] = {}
-      dia[str(diaint+1)][ementa+1]['calorias'] = {}
-      dia[str(diaint+1)][ementa+2]['calorias'] = {}
-      dia[str(diaint+1)][ementa+3]['calorias'] = {}
-      dia[str(diaint+1)][ementa+4]['calorias'] = {}
-      dia[str(diaint+1)][ementa+5]['refeicao'] = {}
-      dia[str(diaint+1)][ementa+1]['refeicao'] = {}
-      dia[str(diaint+1)][ementa+2]['refeicao'] = {}
-      dia[str(diaint+1)][ementa+3]['refeicao'] = {}
-      dia[str(diaint+1)][ementa+4]['refeicao '] = {}
       
-      dia[str(diaint+2)][ementa+5] = {}
-      dia[str(diaint+2)][ementa+1] = {}
-      dia[str(diaint+2)][ementa+2] = {}
-      dia[str(diaint+2)][ementa+3] = {}
-      dia[str(diaint+2)][ementa+4] = {}        
-      dia[str(diaint+2)][ementa+5]['tipo_prato'] = {}
-      dia[str(diaint+2)][ementa+1]['tipo_prato'] = {}
-      dia[str(diaint+2)][ementa+2]['tipo_prato'] = {}
-      dia[str(diaint+2)][ementa+3]['tipo_prato'] = {}
-      dia[str(diaint+2)][ementa+4]['tipo_prato'] = {}        
-      dia[str(diaint+2)][ementa+5]['calorias'] = {}
-      dia[str(diaint+2)][ementa+1]['calorias'] = {}
-      dia[str(diaint+2)][ementa+2]['calorias'] = {}
-      dia[str(diaint+2)][ementa+3]['calorias'] = {}
-      dia[str(diaint+2)][ementa+4]['calorias'] = {}
-      dia[str(diaint+2)][ementa+5]['refeicao'] = {}
-      dia[str(diaint+2)][ementa+1]['refeicao'] = {}
-      dia[str(diaint+2)][ementa+2]['refeicao'] = {}
-      dia[str(diaint+2)][ementa+3]['refeicao'] = {}
-      dia[str(diaint+2)][ementa+4]['refeicao '] = {}
+      for i in range(0,5):
+        dt = datetime.datetime(int(ano),+int(demes),diaint  )+datetime.timedelta(days=i)
+        diai = dt.day
       
-      
-      dia[str(diaint+3)][ementa+1] = {}
-      dia[str(diaint+3)][ementa+2] = {}
-      dia[str(diaint+3)][ementa+3] = {}
-      dia[str(diaint+3)][ementa+4] = {}     
-      dia[str(diaint+3)][ementa+5] = {}   
-      dia[str(diaint+3)][ementa+5]['tipo_prato'] = {}
-      dia[str(diaint+3)][ementa+1]['tipo_prato'] = {}
-      dia[str(diaint+3)][ementa+2]['tipo_prato'] = {}
-      dia[str(diaint+3)][ementa+3]['tipo_prato'] = {}
-      dia[str(diaint+3)][ementa+4]['tipo_prato'] = {}        
-      dia[str(diaint+3)][ementa+5]['calorias'] = {}
-      dia[str(diaint+3)][ementa+1]['calorias'] = {}
-      dia[str(diaint+3)][ementa+2]['calorias'] = {}
-      dia[str(diaint+3)][ementa+3]['calorias'] = {}
-      dia[str(diaint+3)][ementa+4]['calorias'] = {}
-      dia[str(diaint+3)][ementa+5]['refeicao'] = {}
-      dia[str(diaint+3)][ementa+1]['refeicao'] = {}
-      dia[str(diaint+3)][ementa+2]['refeicao'] = {}
-      dia[str(diaint+3)][ementa+3]['refeicao'] = {}
-      dia[str(diaint+3)][ementa+4]['refeicao '] = {}
-      
-      dia[str(diaint+4)][ementa+5] = {}
-      dia[str(diaint+4)][ementa+1] = {}
-      dia[str(diaint+4)][ementa+2] = {}
-      dia[str(diaint+4)][ementa+3] = {}
-      dia[str(diaint+4)][ementa+4] = {}        
-      dia[str(diaint+4)][ementa+5]['tipo_prato'] = {}
-      dia[str(diaint+4)][ementa+1]['tipo_prato'] = {}
-      dia[str(diaint+4)][ementa+2]['tipo_prato'] = {}
-      dia[str(diaint+4)][ementa+3]['tipo_prato'] = {}
-      dia[str(diaint+4)][ementa+4]['tipo_prato'] = {}        
-      dia[str(diaint+4)][ementa+5]['calorias'] = {}
-      dia[str(diaint+4)][ementa+1]['calorias'] = {}
-      dia[str(diaint+4)][ementa+2]['calorias'] = {}
-      dia[str(diaint+4)][ementa+3]['calorias'] = {}
-      dia[str(diaint+4)][ementa+4]['calorias'] = {}
-      dia[str(diaint+4)][ementa+5]['refeicao'] = {}
-      dia[str(diaint+4)][ementa+1]['refeicao'] = {}
-      dia[str(diaint+4)][ementa+2]['refeicao'] = {}
-      dia[str(diaint+4)][ementa+3]['refeicao'] = {}
-      dia[str(diaint+4)][ementa+4]['refeicao '] = {}
-      maxdia=diaint+4
+        dia[str(diai)]= {}        
+        dia[str(diai)][ementa+1] = {}
+        dia[str(diai)][ementa+2] = {}
+        dia[str(diai)][ementa+3] = {}
+        dia[str(diai)][ementa+4] = {}        
+        dia[str(diai)][ementa+5] = {}
+        
+        dia[str(diai)][ementa+1]['tipo_prato'] = {}
+        dia[str(diai)][ementa+2]['tipo_prato'] = {}
+        dia[str(diai)][ementa+3]['tipo_prato'] = {}
+        dia[str(diai)][ementa+4]['tipo_prato'] = {}        
+        dia[str(diai)][ementa+5]['tipo_prato'] = {}
+        
+        dia[str(diai)][ementa+1]['calorias'] = {}
+        dia[str(diai)][ementa+2]['calorias'] = {}
+        dia[str(diai)][ementa+3]['calorias'] = {}
+        dia[str(diai)][ementa+4]['calorias'] = {}
+        dia[str(diai)][ementa+5]['calorias'] = {}
+        
+        dia[str(diai)][ementa+1]['refeicao'] = {}
+        dia[str(diai)][ementa+2]['refeicao'] = {}
+        dia[str(diai)][ementa+3]['refeicao'] = {}
+        dia[str(diai)][ementa+4]['refeicao '] = {}
+        dia[str(diai)][ementa+5]['refeicao'] = {}
+        
+        maxdia=diai
       
     
   for line in textPage.split('\n'):
     line = line.strip()
-    
+    '''bug do parser quando retorna por exemplo Grelhado 721'''    
+    if len(line.split(' '))== 2:
+        tempAr = line.split(' ')
+        if (tempAr[0] in tipo_prato and tempAr[1].isdigit()) or (tempAr[0].isdigit() and tempAr[1] in tipo_prato):
+            parseLine(tempAr[0])
+            parseLine(tempAr[1])
+            continue
+            
     if export_text == True:
       print line
       continue
     else:
       print "->",line
-    if len(line.strip()) == 1:
-      continue
-    if len(line)==0:
-      continue
-    if line.strip() in ignorar:
-      continue
-    if line.strip() in ignorar2:
-      print 'ignorar2 matched. comtinue...'
-      continue      
-    salta = False
-    for word in ignorar2:
-      if line.strip().startswith(word):
-          salta = True
-          break
-    if salta == True:              
-      continue   
-
-    if line.strip() in tipo_prato:
-      numtipo_prato+=1
-      print line          
-      diaint = int(dedia)
-      (diaint,item_linha)=get_dia(diaint,ames,ano,numtipo_prato,line)
-      print "dia[%d][%d]['tipo_prato']"%(diaint,item_linha)
-      dia[str(diaint)][item_linha]['tipo_prato'] = line
-      continue
-      
-    if line.strip() in refeicao:
-      numrefeicao+=1
-      diaint = int(dedia)
-      #print line 
-      (diaint,item_linha)=get_dia(diaint,ames,ano,numrefeicao,line)
-      print "dia[%d][%d]['refeicao']"%(diaint,item_linha)  
-      if diaint > maxdia:
-        '''refeicao tb existe em feriados
-         '''
-        print '%d (dia) > %d (maxdia)'%(diaint,maxdia)
-        continue
-        
-      dia[str(diaint)][item_linha]['refeicao'] = line
-      continue
-  
-    if line.strip().isdigit():
-      decimal+=1
-      diaint = int(dedia)
-      (diaint,item_linha)=get_dia(diaint,ames,ano,decimal,line)
-      print "dia[%d][%d]['calorias']"%(diaint,item_linha)  
-      dia[str(diaint)][item_linha]['calorias'] = line
-      continue
-
-    if last_ementa == line:
-      '''duplicado, bug do parser'''
-      continue
-    ementa+=1
     
-    '''if len(dia.keys()) == 0:
-      print "Vaaaaaaaaaaaaaaaaazio"
-    dia['20'] = {}
-    if len(dia.keys()) == 0:
-      print "Vaaaaaaaaaaaaaaaaazio"
-    dia['20']['21'] = {}
-    dia['20']['21'][ementa] =  line      
-    '''
-    #print i
-    #i+=1
-            
-    #print 'dia',dia
-     
-    #if ementa > 5:
-      #reset
-      #ementa = 1
-      #diaint +=1
-      #diaint = (ementa/5) 
-    diaint = int(dedia)
-    (diaint,ementalinha)=get_dia(diaint,ames,ano,ementa,line)
+    parseLine(line)
     
-    
-    print "dia[%s][%d]['ementa']" % ( str(diaint), ementalinha)
-    dia[str(diaint)][ementalinha]['ementa'] = line
-    last_ementa = line
       
 print'------------------------var dia'      
 print 'dia',dia
